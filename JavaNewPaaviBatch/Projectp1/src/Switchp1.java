06cb0f6f602953729cfae31f1b6ce44c2c772285
azartechnet <azartechnet@gmail.com>
2023-12-21 18:45:58 +0530

    updated

---
+++ JavaNewPaaviBatch/Projectp1/src/Switchp1.java
@@ -0,0 +1,26 @@
+
+public class Switchp1 {
+
+	public static void main(String[] args) {
+		
+		//int exp1=6;
+		String exp1="ones";
+		
+		switch(exp1)
+		{
+		case "ones":
+			System.out.println("Case-1");
+			break;
+		case "twos":
+			System.out.println("Case-2");
+			break;
+		case "threes":
+			System.out.println("Case-3");
+			break;
+		default:
+			System.out.println("default case");
+			break;
+		}
+	}
+
+}
