6f0f4a6917a83f8897af2299b6309c3abc4f4711
azartechnet <azartechnet@gmail.com>
2024-02-05 18:59:19 +0530

    updated

---
+++ JavaNewPaaviBatch/ProjectP2/src/StringBuilderConcepts.java
@@ -0,0 +1,16 @@
+
+public class StringBuilderConcepts {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb=new StringBuilder("Welcome");
+//		System.out.println(sb.append("azar"));
+//		System.out.println(sb.length());
+//		System.out.println(sb.substring(0,5));
+//		System.out.println(sb.charAt(0));
+		System.out.println(sb.indexOf("c",11));
+		System.out.println(sb.lastIndexOf("e", 3));
+
+	}
+
+}
