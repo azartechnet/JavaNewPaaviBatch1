6fb6b3f78202064a54af2fe6a9df94b8b05b253f
azartechnet <azartechnet@gmail.com>
2024-04-09 12:48:44 +0530

    updated

--- JavaNewPaaviBatch/HibernateXmlProgram/src/com/xyz/first/Employee.java
+++ JavaNewPaaviBatch/HibernateXmlProgram/src/com/xyz/first/Employee.java
@@ -1,5 +1,27 @@
 package com.xyz.first;
 
-public class Employee {
-
+public class Employee 
+{
+   private int id;
+   private String firstName,lastName;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+   
+   
 }
