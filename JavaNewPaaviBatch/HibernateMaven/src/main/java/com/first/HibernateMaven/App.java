f680e6a039b48889eec87403f19376ee4945d4d9
azartechnet <azartechnet@gmail.com>
2024-04-30 18:02:16 +0530

    updated

---
+++ JavaNewPaaviBatch/HibernateMaven/src/main/java/com/first/HibernateMaven/App.java
@@ -0,0 +1,13 @@
+package com.first.HibernateMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
